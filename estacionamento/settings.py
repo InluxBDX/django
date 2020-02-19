950ef8e253ea37548bd1c843397bcff445032128
inluxbdx <talles.silva@gmail.com>
2020-02-19 14:26:22 -0300

    Fixing Allowed Hosts

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'estacio-django.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'estacio-django.herokuapp.com']
 
 
 # Application definition
